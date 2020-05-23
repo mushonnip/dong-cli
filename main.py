149d872468559f826d587606cf7cc3f758e580d8
mushonnip <mushonnipabu@gmail.com>
2020-05-23 07:39:16 +0700

    homepage

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ response = requests.get('http://kazefuri.me/')
 soup = BeautifulSoup(response.text, 'html.parser')
 
 posts = soup.find_all(class_='grid3')
-
 for post in posts:
-    print(post)
+    title = post.find('b').get_text().replace('\n', '')
+    link =  post.find('a')['href']
+    if post.find(class_='newepisodefloat right bgwhitetr'):
+        episode = post.find(class_='newepisodefloat right bgwhitetr').get_text().replace('\nEps.', '')
+    print(title, episode, link)
