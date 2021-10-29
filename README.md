4327e61a7acc8836068d85a4e46f973ae961cb58
mushonnip <mushonnipabu@gmail.com>
2021-10-29 15:05:57 +0700

    re-init

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Donghua-Update
+# dong-cli
+Browse and watch Donghua in CLI
+inspired by [ani-cli](https://github.com/pystardust/ani-cli)
+
+This tool scrapes the site [Animexin](https://animexin.xyz)
--- main.py
+++ main.py
@@ -1,18 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
-import tableprint as tp
-import numpy as np
 
-response = requests.get('http://kazefuri.me/')
+response = requests.get('https://animexin.xyz')
 soup = BeautifulSoup(response.text, 'html.parser')
-posts = soup.find_all(class_='grid3')
-data = np.empty((0, 2))
-for post in posts:
-    title = post.find('b').get_text().replace('\n', '')
-    link =  post.find('a')['href']
-    if post.find(class_='newepisodefloat right bgwhitetr'):
-        episode = post.find(class_='newepisodefloat right bgwhitetr').get_text().replace('\nEps.', '')
-    data = np.append(data, np.array([[title, episode]]), axis=0)
 
-headers = ['Judul', 'Episode']
-tp.table(data, headers, align='left')
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
